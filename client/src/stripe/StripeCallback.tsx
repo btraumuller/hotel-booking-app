d3139bc0fc3e9e3856b5fd1982b8a62bc026c87b
btraumuller <btraumuller@gmail.com>
2024-10-24 00:22:20 -0400

    updating callback with account Status function

--- client/src/stripe/StripeCallback.tsx
+++ client/src/stripe/StripeCallback.tsx
@@ -1,6 +1,31 @@
 import {LoadingOutlined} from '@ant-design/icons';
 
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAccountStatus } from '../actions/stripe';
+
 const StripeCallback = ({history}:any) => {
+    const {auth} = useSelector((state:any) => ({...state}));
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        
+        const accountStatus = async () => {
+            
+            try {
+                const res = await getAccountStatus(auth.token);
+                console.log("USER ACCOUNT STATUS ON STRIPE CALLBACK",res);
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        if (auth && auth.token) {
+            accountStatus();
+        }
+    }, [auth]);
+    
     return (
         <div className="d-flex justify-content-center p-5">
             <LoadingOutlined className="display-1 text-danger p-5"/>
