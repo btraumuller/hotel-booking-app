72e93df01a58a1749e8660ec6fdbb9f5c18de6b3
btraumuller <btraumuller@gmail.com>
2024-11-21 20:03:54 -0500

    removing redundancy on hotelFormType

--- client/src/types/hotel.tsx
+++ client/src/types/hotel.tsx
@@ -11,15 +11,7 @@ export type hotelFormType = {
     handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
     handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
     handleImageChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    values: {
-        title: string,
-        content: string,
-        location: string,
-        price: string,
-        from: string,
-        to: string,
-        bed: string
-    },
+    values: hotelFormValues,
     setValues: (values: any) => void,
 }
 
